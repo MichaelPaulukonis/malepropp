643c0eb4de5b37dfeec72c1e7433d15f7649b61c
Michael Paulukonis <xraysmalevich@gmail.com>
2016-03-08 21:03:55 -0500

    config should not have been ignored.

---
+++ config.js
@@ -0,0 +1,14 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+// if you see 'process.env.SOMETHING' that means it's a heroku environment variable
+// heroku plugins:install https://github.com/ddollar/heroku-config.git
+// and will only work with 'foreman start worker'
+module.exports = {
+
+    consumerKey:    process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    accessToken:    process.env.ACCESS_TOKEN,
+    accessSecret:   process.env.ACCESS_SECRET
+
+    // log:                  (process.env.LOG.toLowerCase() === 'true')
+
+};
