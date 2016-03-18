528465487956b80bd52f64777ceae63d9b7572fd
Michael Paulukonis <xraysmalevich@gmail.com>
2016-03-17 21:56:30 -0400

    more modular template test

--- tests/malepropp.tests.js
+++ tests/malepropp.tests.js
@@ -5,8 +5,11 @@ var tester = function() {
   var mocha = require('mocha'),
       chai = require('chai'),
       expect = chai.expect,
-      defaultTemplates = require('../templates.js'),
+      // TODO: okay, make it an object or array
+      slavicTemplates = require('../templates.js'),
       businessTemplates = require('../templates.business.js'),
+      brownTemplates = require('../default.templates.js'),
+      descriptiveTemplates = require('../templates.descriptive.js'),
       wordbank = require('../wordbank.test.js')(require('../words.js')),
       storygen = require('../propp.js'),
       world = storygen().world; // hey, we're assuming this works w/o testing!
@@ -15,7 +18,7 @@ var tester = function() {
   // WHICH IS NOT TESTED PRIOR TO THIS FUNCTION OUCH
   var commonSettings = function(templates) {
 
-    templates = templates || defaultTemplates;
+    templates = templates || slavicTemplates;
 
     var setts = {
       herogender: 'female',
@@ -36,6 +39,17 @@ var tester = function() {
 
   };
 
+  var funcList = [
+    'func0', 'func1', 'func2', 'func3', 'func4',
+    'func5', 'func6', 'func7', 'func8', 'func8a',
+    'func9', 'func10', 'func11', 'func12', 'func13',
+    'func14', 'func15', 'func16', 'func17',
+    'func18', 'func19', 'func20', 'func21',
+    'func22', 'func23', 'func24', 'func25',
+    'func26', 'func27', 'func28', 'func29',
+    'func30', 'func31'
+  ];
+
   describe('presets tests', function() {
 
     it('should provide non-null presets object', function() {
@@ -44,107 +58,63 @@ var tester = function() {
 
   });
 
-  describe('defaultTemplates storyGen functions standalone', function() {
+  describe('different templates function standalone', function() {
 
-    var cs = commonSettings();
-    cs.settings.funcs = [];
+    var templates = {
+      'slavic': slavicTemplates
+      , 'business': businessTemplates
+      // many MANY failures with the below
+      // , 'brown': brownTemplates
+      // , 'descriptive': descriptiveTemplates
+    };
 
-    var funcList = [
-      'func0', 'func1', 'func2', 'func3', 'func4',
-      'func5', 'func6', 'func7', 'func8', 'func8a',
-      'func9', 'func10', 'func11', 'func12', 'func13',
-      'func14', 'func15', 'func16', 'func17',
-      'func18', 'func19', 'func20', 'func21',
-      'func22', 'func23', 'func24', 'func25',
-      'func26', 'func27', 'func28', 'func29',
-      'func30', 'func31'
-    ];
+    var testTemplate = function(name, template) {
+      // TODO: re-work so we can pass in a specific template
+      describe(name + ' storyGen functions standalone', function() {
 
-    funcList.forEach(function(func) {
-      it('correctly generates text for ' + func, function() {
-        cs.settings.funcs = [func];
-        var sg = new storygen(cs.settings);
-        var story = sg.generate(cs.settings, cs.theme);
-        expect(story.tale).to.not.be.null;
-        expect(story.tale).to.have.length.above(10);
-        expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
-        // console.log(func, ': ', story.tale);
-      });
-    });
+        var cs = commonSettings(template);
+        cs.settings.funcs = [];
 
-
-    describe('defaultTemplates func8 subfuntions', function() {
-
-      it('exposes the subfunctions of func8', function() {
-        expect(world.func8subfuncs).to.not.be.null;
-        expect(typeof world.func8subfuncs).to.equal('object');
-      });
-
-      var subs = world.func8subfuncs;
-      for(var subfunc in subs) {
-        it('generates text for func8, subfunc: ' + subs[subfunc], function() {
-          cs.settings.funcs = ['func8', subs[subfunc]];
-          var sg = new storygen(cs.settings);
-          var story = sg.generate(cs.settings, cs.theme);
-          expect(story.tale).to.not.be.null;
-          expect(story.tale).to.have.length.above(10);
-          expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
+        funcList.forEach(function(func) {
+          it('correctly generates text for ' + func, function() {
+            cs.settings.funcs = [func];
+            var sg = new storygen(cs.settings);
+            var story = sg.generate(cs.settings, cs.theme);
+            expect(story.tale).to.not.be.null;
+            expect(story.tale).to.have.length.above(10);
+            expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
+            // console.log(func, ': ', story.tale);
+          });
         });
-      };
-    });
 
-  });
+        describe(name + ' func8 subfuntions', function() {
 
+          it('exposes the subfunctions of func8', function() {
+            expect(world.func8subfuncs).to.not.be.null;
+            expect(typeof world.func8subfuncs).to.equal('object');
+          });
 
-
-
-
-  describe('businesstemplate storyGen functions standalone', function() {
-
-    var cs = commonSettings(businessTemplates);
-    cs.settings.funcs = [];
-
-    var funcList = [
-      'func0', 'func1', 'func2', 'func3', 'func4',
-      'func5', 'func6', 'func7', 'func8', 'func8a',
-      'func9', 'func10', 'func11', 'func12', 'func13',
-      'func14', 'func15', 'func16', 'func17',
-      'func18', 'func19', 'func20', 'func21',
-      'func22', 'func23', 'func24', 'func25',
-      'func26', 'func27', 'func28', 'func29',
-      'func30', 'func31'
-    ];
-
-    funcList.forEach(function(func) {
-      it('correctly generates text for ' + func, function() {
-        cs.settings.funcs = [func];
-        var sg = new storygen(cs.settings);
-        var story = sg.generate(cs.settings, cs.theme);
-        expect(story.tale).to.not.be.null;
-        expect(story.tale).to.have.length.above(10);
-        expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
-      });
-    });
-
-    describe('defaultTemplates func8 subfuntions', function() {
-
-      it('exposes the subfunctions of func8', function() {
-        expect(world.func8subfuncs).to.not.be.null;
-        expect(typeof world.func8subfuncs).to.equal('object');
-      });
-
-      var subs = world.func8subfuncs;
-      for(var subfunc in subs) {
-        it('generates text for func8, subfunc: ' + subs[subfunc], function() {
-          cs.settings.funcs = ['func8', subs[subfunc]];
-          var sg = new storygen(cs.settings);
-          var story = sg.generate(cs.settings, cs.theme);
-          expect(story.tale).to.not.be.null;
-          expect(story.tale).to.have.length.above(10);
-          expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
+          var subs = world.func8subfuncs;
+          for(var subfunc in subs) {
+            it('generates text for func8, subfunc: ' + subs[subfunc], function() {
+              cs.settings.funcs = ['func8', subs[subfunc]];
+              var sg = new storygen(cs.settings);
+              var story = sg.generate(cs.settings, cs.theme);
+              expect(story.tale).to.not.be.null;
+              expect(story.tale).to.have.length.above(10);
+              expect(story.title).to.have.length.above(5); // some have come in at 10. Maybe less is possible.
+              // console.log(subfunc, ': ', story.tale);
+            });
+          };
         });
-      };
-    });
+
+      });
+
+    };
+
+    for (var name in templates) {
+      testTemplate(name, templates[name]);
+    }
 
   });
 
