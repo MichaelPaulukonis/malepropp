1f8f3407317fb4c24ed2b873ebbaa48c1d77b17a
MichaelPaulukonis <xraysmalevich@gmail.com>
2026-08-16 00:48:10 -0400

    Updates

--- gui.js
+++ gui.js
@@ -1,256 +1,339 @@
-var gui = function() {
+var gui = (function () {
+  // TODO: these are information popups
+  // grab the information from the original, or rewrite
+  var popup = function (url) {
+    window.open(
+      url + ".html",
+      "win",
+      "toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1,width=300,height=300",
+    );
+  };
 
-    // TODO: these are information popups
-    // grab the information from the original, or rewrite
-    var popup = function(url) {
-        window.open(url+".html", "win", "toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1,width=300,height=300");
-    };
+  var popup2 = function (url) {
+    window.open(
+      url + ".html",
+      "win",
+      "toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1,width=350,height=400",
+    );
+  };
 
-    var popup2 = function(url) {
-        window.open(url+".html", "win", "toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1,width=350,height=400");
-    };
+  var setall = function (toggle) {
+    var funcs = document.querySelectorAll("input[type=checkbox]");
+    funcs.forEach(function (element) {
+      element.checked = toggle;
+    });
+  };
 
-    var setall = function(toggle) {
-        var funcs = $('input[type=checkbox]');
-        funcs.each(function(index, element) {
-            $(element).prop('checked', toggle);
-        });
-    };
+  var randomize = function () {
+    var funcs = document.querySelectorAll("input[type=checkbox]");
+    funcs.forEach(function (element) {
+      element.checked = Math.random() < 0.5;
+    });
+  };
 
-    return {
-        popup: popup,
-        popup2: popup2,
-        setall: setall
-    };
+  return {
+    popup: popup,
+    popup2: popup2,
+    setall: setall,
+    randomize: randomize,
+  };
+})();
 
-}();
+var preset = function (presets) {
+  var propp = storyGen.resetProppFunctions(false); // ARGH these are all now true!
 
-
-var preset = function(presets) {
-
-    var propp = storyGen.resetProppFunctions(false); // ARGH these are all now true!
-
-    for (var i = 0; i < presets.functions.length; i++) {
-
-        var func = presets.functions[i];
-        var subFunc;
-        if (typeof func === 'object') {
-            subFunc = func[1];
-            func = func[0];
-        }
-        // sadly, we are discarding the subfuntion in the gui
-        // so altough we can fine-grain a preset, we can't use it in the gui...
-        // we can't just say "use the preset if selected"
-        // becasue we wan't to play with the presets, adding and subtracting in the gui
-        // SO: need to store sub-funcs in the gui
-        // and why not make them selectable, then....
-        propp[func].active = true;
-
-
+  for (var i = 0; i < presets.functions.length; i++) {
+    var func = presets.functions[i];
+    var subFunc;
+    if (typeof func === "object") {
+      subFunc = func[1];
+      func = func[0];
     }
+    // sadly, we are discarding the subfuntion in the gui
+    // so altough we can fine-grain a preset, we can't use it in the gui...
+    // we can't just say "use the preset if selected"
+    // becasue we wan't to play with the presets, adding and subtracting in the gui
+    // SO: need to store sub-funcs in the gui
+    // and why not make them selectable, then....
+    propp[func].active = true;
+  }
 
-    return propp;
+  return propp;
 };
 
+var pushPreset = function (setname) {
+  if (!storyGen.presets[setname]) {
+    return;
+  }
 
-var pushPreset = function(setname) {
-    if (!storyGen.presets[setname]) { return; }
-
-    var story = preset(storyGen.presets[setname]);
-    // pushSettingsToGui(story);
-    pushSettingsToGuiNew(storyGen.presets[setname]);
-
+  var story = preset(storyGen.presets[setname]);
+  // pushSettingsToGui(story);
+  pushSettingsToGuiNew(storyGen.presets[setname]);
 };
 
 // old-fashioned method
-var pushSettingsToGui = function(proppFunctions) {
+var pushSettingsToGui = function (proppFunctions) {
+  for (var index in proppFunctions) {
+    window.document.myform[index].checked = proppFunctions[index].active;
+  }
 
-    for (var index in proppFunctions) {
-        window.document.myform[index].checked = proppFunctions[index].active;
-    }
-
-    // set the radios
-    // http://stackoverflow.com/questions/871063/how-to-set-radio-option-checked-onload-with-jquery
-
+  // set the radios
+  // http://stackoverflow.com/questions/871063/how-to-set-radio-option-checked-onload-with-jquery
 };
-
 
 // take in array of functions
 // if element is string, it is an active function
 // if element is array, it is a function with a sub-Function
 // further parameters not yet handled
 // radio button options (globals) not yet handled
-var pushSettingsToGuiNew = function(funcs, toggle) {
+var pushSettingsToGuiNew = function (funcs, toggle) {
+  toggle = toggle || true;
 
-    // TODO: clear the settings
-    toggle = toggle || true;
+  gui.setall(false);
 
-    for (var index in funcs.functions) {
-
-        var func = funcs.functions[index];
-        var subFunc;
-        if (typeof func === 'object') {
-            subFunc = func[1];
-            func = func[0];
-        }
-
-        window.document.myform[func].checked = toggle;
-
-        if (subFunc) {
-            var id;
-
-            switch (func) {
-            case 'func8':
-                id = 'func8subfunc';
-                break;
-            }
-
-            if (id) {
-                $('#' + id).val(subFunc);
-            }
-
-        }
-
+  for (var index in funcs.functions) {
+    var func = funcs.functions[index];
+    var subFunc;
+    if (typeof func === "object") {
+      subFunc = func[1];
+      func = func[0];
     }
 
-    window.document.myform.bossfight.checked = funcs.bossfight;
+    window.document.myform[func].checked = toggle;
 
+    if (subFunc) {
+      var id;
+
+      switch (func) {
+        case "func8":
+          id = "func8subfunc";
+          break;
+      }
+
+      if (id) {
+        document.getElementById(id).value = subFunc;
+      }
+    }
+  }
+
+  window.document.myform.bossfight.checked = funcs.bossfight;
 };
 
-var getFunctionsFromGui = function() {
+var getFunctionsFromGui = function () {
+  // proppFunctions is defined in propp.js
+  // this is an external dependency to the GUI
+  // to the extant that without that file, the GUI has no purpose
+  // propp.js should be able to function w/o the GUI, however...
+  var funcs = storyGen.resetProppFunctions();
+  var f = [];
+  for (var index in funcs) {
+    funcs[index].active = window.document.myform[index].checked;
+  }
 
-    // proppFunctions is defined in propp.js
-    // this is an external dependency to the GUI
-    // to the extant that without that file, the GUI has no purpose
-    // propp.js should be able to function w/o the GUI, however...
-    var funcs = storyGen.resetProppFunctions();
-    var f = [];
-    for (var index in funcs) {
-        funcs[index].active = window.document.myform[index].checked;
-    }
+  var herog = document.querySelector(
+    "input[name=herogender][type=radio]:checked",
+  ).value;
+  var villaing = document.querySelector(
+    "input[name=villaingender][type=radio]:checked",
+  ).value;
+  var peopleg = document.querySelector(
+    "input[name=peoplegender][type=radio]:checked",
+  ).value;
+  var bossfight = window.document.myform.bossfight.checked;
 
-    var herog = $('input[name=herogender][type=radio]:checked').val();
-    var villaing = $('input[name=villaingender][type=radio]:checked').val();
-    var peopleg = $('input[name=peoplegender][type=radio]:checked').val();
-    var bossfight = window.document.myform.bossfight.checked;
+  // TOO LATE - not in the array, which has to be in order. DANG.
+  // funcs = storyGen.enforceRules(funcs);
+  pushSettingsToGui(funcs);
 
-    // TOO LATE - not in the array, which has to be in order. DANG.
-    // funcs = storyGen.enforceRules(funcs);
-    pushSettingsToGui(funcs);
-
-    // this is awkward....
-    // and how do we handle sub-funcs???
-    for (index in funcs) {
-        if (window.document.myform[index].checked) {
-            if (index === 'func8') {
-                var subFunc = 'func8subfunc';
-                var sf = $('#' + subFunc).val();
-                var sfv;
-                if (sf.toLowerCase() !== 'random') {
-                    sfv = sf;
-                    f.push([index, sfv]);
-                } else {
-                    f.push(index);
-                }
-            } else {
-                f.push(index);
-            }
-
+  // this is awkward....
+  // and how do we handle sub-funcs???
+  for (index in funcs) {
+    if (window.document.myform[index].checked) {
+      if (index === "func8") {
+        var subFunc = "func8subfunc";
+        var sf = document.getElementById(subFunc).value;
+        var sfv;
+        if (sf.toLowerCase() !== "random") {
+          sfv = sf;
+          f.push([index, sfv]);
+        } else {
+          f.push(index);
         }
+      } else {
+        f.push(index);
+      }
     }
+  }
 
-    return {
-        herogender: herog,
-        villaingender: villaing,
-        peoplegender: peopleg,
-        functions: funcs,   // object with [funcn].active
-        funcs: f,           // array-based list
-        bossfight: bossfight
-    };
-
+  return {
+    herogender: herog,
+    villaingender: villaing,
+    peoplegender: peopleg,
+    functions: funcs, // object with [funcn].active
+    funcs: f, // array-based list
+    bossfight: bossfight,
+  };
 };
 
-var shoveToGui = function(tale) {
-
-    window.document.myform.output.value = tale.title.toUpperCase() + '\n\n' + tale.tale;
-
+var shoveToGui = function (tale) {
+  // not every theme defines a title generator (eg. descriptive/skeleton
+  // theme) - tale.title can be undefined
+  var title = tale.title ? tale.title.toUpperCase() : "(untitled)";
+  window.document.myform.output.value = title + "\n\n" + tale.tale;
 };
 
-var guiGet = function() {
+var guiGet = function () {
+  var settings = getFunctionsFromGui();
 
-    var settings = getFunctionsFromGui();
+  settings.conclusion = true;
 
-    settings.conclusion = true;
+  var selectedTheme = document.querySelector(
+    "input[name=theme][type=radio]:checked",
+  ).value;
+  settings.verbtense = document.querySelector(
+    "input[name=tense][type=radio]:checked",
+  ).value;
 
-    var selectedTheme = $('input[name=theme][type=radio]:checked').val();
-    settings.verbtense = $('input[name=tense][type=radio]:checked').val();
+  var theme = {};
+  switch (selectedTheme) {
+    case "office":
+      theme = {
+        bank: businessbank(words),
+        templates: businessTemplates,
+      };
+      break;
 
-    var theme = {};
-    switch(selectedTheme) {
-    case 'office':
-        theme = {
-            bank: businessbank(words),
-            templates: businessTemplates
-        };
-        break;
+    case "test":
+      theme = {
+        bank: defaultbank(words),
+        templates: nTemplates,
+      };
+      break;
 
-    case 'test':
-        theme = {
-            bank: defaultbank(words),
-            templates: nTemplates
-        };
-        break;
+    case "descriptive":
+      theme = {
+        bank: defaultbank(words),
+        templates: descriptiveTemplates,
+      };
+      break;
 
-    case 'original':
-        theme = {
-            bank: defaultbank(words), // although it won't be used...
-            templates: defaultTemplates
-        };
-    };
+    case "original":
+      theme = {
+        bank: defaultbank(words), // although it won't be used...
+        templates: defaultTemplates,
+      };
+  }
 
-    // STILL EXPECTS THE story['funcn'].active stuff to be present. OUTCH
-    // WHY ARE WE PASSING THE SETTINGS IN TWICE ?!?!!?!
-    var sg = new storyGen(settings);
-    var tale = sg.generate(settings, theme);
+  // STILL EXPECTS THE story['funcn'].active stuff to be present. OUTCH
+  // WHY ARE WE PASSING THE SETTINGS IN TWICE ?!?!!?!
+  var sg = new storyGen(settings);
+  var tale = sg.generate(settings, theme);
 
-    shoveToGui(tale);
+  shoveToGui(tale);
 };
 
-$('#selectall').click(function() {
+// TODO: no element with id="selectall" exists in index.html - this has
+// always been dead code (jQuery no-op'd silently on it; guarded here to
+// keep that same behavior). Either add the element or delete this block.
+var selectAllBtn = document.getElementById("selectall");
+if (selectAllBtn) {
+  selectAllBtn.addEventListener("click", function () {
     gui.setall(true);
-});
-
-
+  });
+}
 
 // TODO: use the preset when generating
 // no, when selected update the gui....
 // and then get rid of the other links
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function () {
+  var inp = document.getElementById("presets");
+  var ps = storyGen.presets;
 
-    var inp = $('#presets');
-    var ps = storyGen.presets;
-    inp.append($('<option />').val('unselected').text('-- Select a preset --'));
-    inp.append($('<option />').val('selectall').text('Select all'));
-    $.each(ps, function(key) {
-        inp.append($('<option />').val(key).text(key));
+  var unselectedOpt = document.createElement("option");
+  unselectedOpt.value = "unselected";
+  unselectedOpt.textContent = "-- Select a preset --";
+  inp.appendChild(unselectedOpt);
+
+  var selectallOpt = document.createElement("option");
+  selectallOpt.value = "selectall";
+  selectallOpt.textContent = "Select all";
+  inp.appendChild(selectallOpt);
+
+  Object.keys(ps).forEach(function (key) {
+    var opt = document.createElement("option");
+    opt.value = key;
+    opt.textContent = key;
+    inp.appendChild(opt);
+  });
+
+  inp.addEventListener("change", function () {
+    var preset = this.value;
+    if (preset === "unselected") {
+      gui.setall(false);
+    } else if (preset === "selectall") {
+      gui.setall(true);
+    } else if (preset !== "manual") {
+      pushPreset(preset);
+    }
+  });
+
+  var randomBtn = document.getElementById("randomize");
+  if (randomBtn) {
+    randomBtn.addEventListener("click", function () {
+      inp.value = "unselected";
+      gui.randomize();
     });
+  }
 
-    inp.change(function() {
-        var preset = $(this).val();
-        if (preset === 'unselected') {
-            gui.setall(false);
-        } else if (preset === 'selectall') {
-            gui.setall(true);
-        } else if (preset !== 'manual') {
-            pushPreset(preset);
-        }
+  var clearBtn = document.getElementById("clear");
+  if (clearBtn) {
+    clearBtn.addEventListener("click", function () {
+      inp.value = "unselected";
+      gui.setall(false);
+      document.getElementById("func8subfunc").selectedIndex = 0;
+      document.getElementById("r1").checked = true;
+      document.getElementById("herogenderfemale").checked = true;
+      document.getElementById("villaingenderfemale").checked = true;
+      document.getElementById("peoplegenderfemale").checked = true;
+      document.getElementById("tensepast").checked = true;
+      window.document.myform.output.value = "";
     });
+  }
 
-    var s = $('#func8subfunc');
-    var sf = world.func8subfuncs;
-    $.each(world.func8subfuncs, function(key,value) {
-        s.append($('<option />').val(value).text(value));
+  var THEME_STORAGE_KEY = "malepropp-theme";
+
+  var themeToggleBtn = document.getElementById("theme-toggle");
+  if (themeToggleBtn) {
+    var updateThemeToggleLabel = function () {
+      var isDark =
+        document.documentElement.getAttribute("data-theme") === "dark";
+      themeToggleBtn.textContent = isDark
+        ? "Switch to light"
+        : "Switch to dark";
+    };
+    updateThemeToggleLabel();
+
+    themeToggleBtn.addEventListener("click", function () {
+      var isDark =
+        document.documentElement.getAttribute("data-theme") === "dark";
+      if (isDark) {
+        document.documentElement.removeAttribute("data-theme");
+      } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+      }
+      try {
+        localStorage.setItem(THEME_STORAGE_KEY, isDark ? "light" : "dark");
+      } catch (e) {}
+      updateThemeToggleLabel();
     });
+  }
 
-
+  var s = document.getElementById("func8subfunc");
+  Object.keys(world.func8subfuncs).forEach(function (key) {
+    var value = world.func8subfuncs[key];
+    var opt = document.createElement("option");
+    opt.value = value;
+    opt.textContent = value;
+    s.appendChild(opt);
+  });
 });
